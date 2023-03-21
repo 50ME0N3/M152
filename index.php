2be6afd5ed88e871a5d059daa73ca57382a56b44
Antoine <antoine.dvt@eduge.ch>
2023-03-21 11:18:25 +0100

    ajout de l'ajax, du resize (seulement pour l'ajax) et de la suppression des fichier qui ne sont soit plus en base ou plus en local

--- index.php
+++ index.php
@@ -16,9 +16,10 @@ include 'models/Post.php';
 include 'models/Media.php';
 
 // afichage du header
-if($uc != "getAllPosts"){
-include 'vues/header.php';
+if ($uc != "getAllPosts") {
+    include 'vues/header.php';
 }
+
 
 // Gestion des affichages
 switch ($uc) {
@@ -27,15 +28,16 @@ switch ($uc) {
         $posts = Post::getAllPosts();
         include "vues/home.php"; // affiche la vue d'accueil
         break;
-        // redirection sur le controller post
+        // redirection sur le controller post   
     case 'post':
         include 'controllers/post_controller.php';
         break;
-
+    case 'deleteAllFiles':
+        Media::deleteAllFilesThatAreNotInBase();
+        break;
 }
-
 // Affichage du footer
-if($uc != "getAllPosts"){
-include 'vues/footer.php';
+if ($uc != "getAllPosts") {
+    include 'vues/footer.php';
 }
 error_reporting(E_ALL);
